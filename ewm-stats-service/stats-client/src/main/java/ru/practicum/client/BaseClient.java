fa8c5a59b1aaaae9e39b3f6d2c51b514e1609e6f
Sergey <sergey.mishakov42@gmail.com>
2023-07-02 23:30:08 +0300

    Diploma stage2_v2

--- ewm-stats-service/stats-client/src/main/java/ru/practicum/client/BaseClient.java
+++ ewm-stats-service/stats-client/src/main/java/ru/practicum/client/BaseClient.java
@@ -97,13 +97,9 @@ public class BaseClient {
         return prepareGatewayResponse(ewmServerResponse);
     }
 
-    private <T> ResponseEntity<StatDto[]> makeAndSendRequestList(HttpMethod method, String path, Long userId, @Nullable Map<String, Object> parameters, @Nullable T body) {
+    private <T> ResponseEntity<StatDto[]> makeAndSendRequestList(HttpMethod method, String path, Long userId, Map<String, Object> parameters, @Nullable T body) {
         HttpEntity<T> requestEntity = new HttpEntity<>(body, defaultHeaders(userId));
-
-        /*ResponseEntity<StatDto[]> responseEntity =
-                rest.getForEntity(path, StatDto[].class);*/
         ResponseEntity<StatDto[]> ewmServerResponse = rest.exchange(path, method, requestEntity, StatDto[].class, parameters);
-
         return prepareGatewayResponseList(ewmServerResponse);
     }
 
