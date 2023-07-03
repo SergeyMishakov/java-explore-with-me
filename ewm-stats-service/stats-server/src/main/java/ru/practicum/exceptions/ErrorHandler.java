3e065f6c14d67e4109ef3cf248c148f119eb4f00
Sergey <sergey.mishakov42@gmail.com>
2023-07-03 23:04:37 +0300

    Diploma stage2_v7

--- ewm-stats-service/stats-server/src/main/java/ru/practicum/exceptions/ErrorHandler.java
+++ ewm-stats-service/stats-server/src/main/java/ru/practicum/exceptions/ErrorHandler.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import java.util.Map;
-
 @RestControllerAdvice
 public class ErrorHandler {
 
@@ -17,31 +15,4 @@ public class ErrorHandler {
     public ValidationException handle(final ValidationException e) {
         return new ValidationException();
     }
-
-    /*@ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public NotFoundException handle(final NotFoundException e) {
-        return new NotFoundException();
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public RuntimeException handle(final RuntimeException e) {
-        return new RuntimeException(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.CONFLICT)
-    @ResponseBody
-    public ConflictException handle(final ConflictException e) {
-        return new ConflictException(e.getMessage());
-    }
-
-    @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ResponseBody
-    public Map<String, String> handle(final IllegalStateException e) {
-        return Map.of("error", e.getMessage());
-    }*/
 }
